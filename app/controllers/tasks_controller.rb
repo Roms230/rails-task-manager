33ff54115f85104342331765451a9727f3381cf1
Romain Thiboudois <romainthiboudois@gmail.com>
2022-10-04 16:36:55 +0400

    done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,25 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to root_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+end
